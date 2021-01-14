ede75f4faea0ec018ca79d444940b4b60a924a92
Jorge Ivan Reyes Valencia <jorgeivanreyesvalencia92@gmail.com>
2021-01-14 17:20:37 -0500

    Refactor

--- tools/grahpql.js
+++ tools/grahpql.js
@@ -1,6 +1,5 @@
-function createUserMutacion(apellidos, correo, estado, fecha_creacion, fk_cargo, fk_dependencia, id) {
-    let mutation = {
-        insert_usuario_one(object: {apellidos: apellidos, correo: correo, estado: estado, fecha_creacion: fecha_creacion, fk_cargo: fk_cargo, fk_dependencia: fk_dependencia, id: id, nombres: nombres}) {
+function createUserMutacion( nombres,apellidos, correo, estado, fk_cargo, fk_dependencia, identificacion) {
+    let mutation = `insert_usuario_one(object: {apellidos: ${apellidos}, correo: ${correo}, estado: ${estado}, fecha_creacion: ${Date.now()}, fk_cargo: ${fk_cargo}, fk_dependencia: ${fk_dependencia}, id: ${identificacion}, nombres: ${nombres}) {
           apellidos
           correo
           estado
@@ -8,8 +7,6 @@ function createUserMutacion(apellidos, correo, estado, fecha_creacion, fk_cargo,
           fk_cargo
           fk_dependencia
           id
-          nombres
-        }
-      }
+          nombres`
     return mutation;
 }
